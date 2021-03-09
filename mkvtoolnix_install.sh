aa34d9d532504a8081b40840e54b3cc61f77fc4b
Raphaël Boulc'h <raphaelboulch@gmail.com>
2021-03-09 15:48:36 +0100

    Update mkvtoolnix_install.sh

--- mkvtoolnix_install.sh
+++ mkvtoolnix_install.sh
@@ -1,5 +1,5 @@
 sudo apt install -y wget && \
-wget https://mkvtoolnix.download/gpg-pub-moritzbunkus.txt -O - | sudo apt-key add - && \
-echo -e "deb https://mkvtoolnix.download/debian/ $(lsb_release -cs) main\ndeb-src https://mkvtoolnix.download/debian/ $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/mkvtoolnix.list && \
+sudo wget https://mkvtoolnix.download/gpg-pub-moritzbunkus.gpg -O /usr/share/keyrings/gpg-pub-moritzbunkus.gpg && \
+echo -e "deb [signed-by=/usr/share/keyrings/gpg-pub-moritzbunkus.gpg] https://mkvtoolnix.download/debian/  "$(lsb_release -cs)" main\ndeb-src [signed-by=/usr/share/keyrings/gpg-pub-moritzbunkus.gpg] https://mkvtoolnix.download/debian/ "$(lsb_release -cs)" main" | sudo tee /etc/apt/sources.list.d/mkvtoolnix.list && \
 sudo apt update && \
 sudo apt install -y apt-transport-https mkvtoolnix mkvtoolnix-gui
